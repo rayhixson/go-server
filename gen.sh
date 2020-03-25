05b4f77d3f0d5e9ae03142d1e0ebd8ce096a5007
Ray <rhixson@treetopllc.com>
2020-03-25 15:52:09 -0700

    zip

--- gen.sh
+++ gen.sh
@@ -1,9 +1,3 @@
 #!/bin/zsh
 
-count=10000000
-while ( [[ $count -le 100000000 ]] ); do
-    echo $count
-    # echo $count | nc 127.0.0.1 1111
-    let count++
-done
-							 
+for count in {1..10000000}; printf "%09d\n" $[RANDOM]$[RANDOM] >> bestnums5.txt
